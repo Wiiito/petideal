6d0102663625fe3c4bb4da1e56de5b3c866e58fd
Wiito <tminemcraft123@gmail.com>
2025-01-20 17:10:11 -0300

    Profile Fix

--- src/app/(userHeader)/profile/page.jsx
+++ src/app/(userHeader)/profile/page.jsx
@@ -23,6 +23,12 @@ const Page = () => {
 
 	return (
 		<>
+			<button
+				className='absolute top-4 right-4 w-32 h-9 content-center rounded-full font-semibold bg-reallyLight text-white opacity-80 hover:opacity-100 transition-all duration-300 ease-in-out z-50'
+				onClick={signOut}
+			>
+				Sair
+			</button>
 			<div className='fixed h-screen w-screen top-0 left-0 -z-10'>
 				<Image
 					src='/authPageBackground.svg'
@@ -39,14 +45,14 @@ const Page = () => {
 				</div>
 				<form className='w-full p-4' action={handleSubmit}>
 					<div className='mt-4 lg:text-center'>
-						<h5>Nome:</h5>
-						<div className='w-full flex justify-center lg:max-w-96 lg:transform lg:-translate-x-1/2 lg:ml-[50%]'>
+						<div className='w-full items-center flex justify-center lg:max-w-96 lg:transform lg:-translate-x-1/2 lg:ml-[50%]'>
+							Nome:
 							<input
 								type='text'
 								defaultValue={
 									status === 'authenticated' ? session.user.name : 'Carregando'
 								}
-								className='border-b focus:border-primary border-transparent outline-none text-2xl w-full bg-trueWhite bg-opacity-35 rounded-full px-3 py-1 backdrop-blur-md'
+								className='border-b focus:border-primary border-transparent outline-none text-2xl w-full bg-trueWhite bg-opacity-35 px-3 py-1 backdrop-blur-md'
 								id='name'
 							/>
 							<label
@@ -58,14 +64,14 @@ const Page = () => {
 						</div>
 					</div>
 					<div className='mt-4 lg:text-center'>
-						<h5>Email:</h5>
-						<div className='w-full flex justify-center lg:max-w-96 lg:transform lg:-translate-x-1/2 lg:ml-[50%]'>
+						<div className='w-full items-center flex justify-center lg:max-w-96 lg:transform lg:-translate-x-1/2 lg:ml-[50%]'>
+							Email:
 							<input
 								type='text'
 								defaultValue={
 									status === 'authenticated' ? session.user.email : 'Carregando'
 								}
-								className='border-b focus:border-primary border-transparent outline-none text-xl w-full bg-trueWhite bg-opacity-35 rounded-full px-3 py-1 backdrop-blur-md'
+								className='border-b focus:border-primary border-transparent outline-none text-xl w-full bg-trueWhite bg-opacity-35 px-3 py-1 backdrop-blur-md'
 								id='email'
 							/>
 							<label
@@ -77,11 +83,11 @@ const Page = () => {
 						</div>
 					</div>
 					<div className='mt-4 lg:text-center'>
-						<h5>Senha:</h5>
-						<div className='w-full flex justify-center lg:max-w-96 lg:transform lg:-translate-x-1/2 lg:ml-[50%]'>
+						<div className='w-full items-center flex justify-center lg:max-w-96 lg:transform lg:-translate-x-1/2 lg:ml-[50%]'>
+							Senha:
 							<input
 								type='password'
-								className='border-b focus:border-primary border-transparent outline-none text-xl w-full bg-trueWhite bg-opacity-35 rounded-full px-3 py-1 backdrop-blur-md'
+								className='border-b focus:border-primary border-transparent outline-none text-xl w-full bg-trueWhite bg-opacity-35 px-3 py-1 backdrop-blur-md'
 								id='password'
 								placeholder='**********'
 							/>
@@ -93,24 +99,20 @@ const Page = () => {
 							</label>
 						</div>
 					</div>
-					<button
-						type='submit'
-						className='mt-4 ml-[50%] transform -translate-x-1/2 border-primary border bg-trueWhite bg-opacity-30 backdrop-blur-sm rounded-xl px-16 py-2'
-					>
-						Salvar Alterações
-					</button>
+					<div className='flex justify-center'>
+						<button
+							type='submit'
+							className='mt-4 border-primary border bg-trueWhite bg-opacity-30 backdrop-blur-sm rounded-xl px-16 py-2'
+						>
+							Salvar Alterações
+						</button>
+					</div>
 				</form>
 				<Link href='/form'>
 					<div className='border-primary border bg-trueWhite bg-opacity-30 backdrop-blur-sm rounded-xl px-16 py-2 mb-4'>
 						Refazer formulario
 					</div>
 				</Link>
-				<button
-					className='border-error border px-16 py-2 rounded-xl font-bold bg-error bg-opacity-20 backdrop-blur-md cursor-pointer hover:bg-opacity-30 transition-all duration-200 mb-4'
-					onClick={signOut}
-				>
-					Sair
-				</button>
 			</div>
 		</>
 	)
